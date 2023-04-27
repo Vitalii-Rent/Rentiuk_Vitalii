0cf0f8a91bcf21f6087c4a5a6603625c8e9aae07
Vitalii-Rent <98947625+Vitalii-Rent@users.noreply.github.com>
2023-04-27 12:04:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ from task5 import sorter
 from task6 import next_bigger
 
 
-
+print(str_filter([1, 2, 'a', 'b']))
+print(first_non_repeating_letter('stress'))
+print(digital_root(123456))
+print(count_pairs_with_sum([1, 3, 6, 2, 2, 0, 4, 5], 5))
+s = "Fired:Corwill;Wilfred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
+print(sorter(s))
+print(next_bigger(513))
 
 
 
